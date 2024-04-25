e5a76da0ff702afbc2734298d22de2184b88bcdd
teledependent71 <93117032+teledependent71@users.noreply.github.com>
2024-04-25 10:28:11 +0300

    Commit from TeleportHQ

---
+++ pages/test-page.js
@@ -0,0 +1,76 @@
+import React from 'react'
+import Head from 'next/head'
+
+import { DataProvider, Repeater } from '@teleporthq/react-components'
+import { getEntityByAttribute } from '@teleporthq/cms-mappers/caisy'
+
+const TestPage = (props) => {
+  return (
+    <>
+      <div className="test-page-container">
+        <Head>
+          <title>test-page - Senior Web Planner</title>
+          <meta property="og:title" content="test-page - Senior Web Planner" />
+        </Head>
+        <DataProvider
+          renderSuccess={(context_72n6tl) => (
+            <>
+              <h1 id={context_72n6tl?.id}>Heading</h1>
+            </>
+          )}
+          params={{
+            projectId: '3bd8eb33-2aaa-4620-87bf-d7ccd04d0245',
+            query:
+              'query MyQuery{TypeWithRichText{_meta{createdAt updatedAt id}title content{json connections{__typename  }}}}',
+            attribute: 'id',
+            id: '2',
+          }}
+          initialData={props.context72n6tlProp}
+          persistDataDuringLoading={true}
+        />
+      </div>
+      <style jsx>
+        {`
+          .test-page-container {
+            width: 100%;
+            display: flex;
+            overflow: auto;
+            min-height: 100vh;
+            align-items: center;
+            flex-direction: column;
+          }
+        `}
+      </style>
+    </>
+  )
+}
+
+export default TestPage
+
+export async function getStaticProps(context) {
+  try {
+    const context72n6tlProp = await getEntityByAttribute({
+      ...context?.params,
+      projectId: '3bd8eb33-2aaa-4620-87bf-d7ccd04d0245',
+      query:
+        'query MyQuery{TypeWithRichText{_meta{createdAt updatedAt id}title content{json connections{__typename  }}}}',
+      attribute: 'id',
+      id: '2',
+    })
+    if (!context72n6tlProp?.data?.[0]) {
+      return {
+        notFound: true,
+      }
+    }
+    return {
+      props: {
+        context72n6tlProp: context72n6tlProp?.data?.[0],
+      },
+      revalidate: 60,
+    }
+  } catch (error) {
+    return {
+      notFound: true,
+    }
+  }
+}
